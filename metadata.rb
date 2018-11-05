f19c742e50cb8e574fde12db8356dad75d074b2d
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-11-05 13:53:40 +0000

    created and tested python cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name 'python_cookbok'
+name 'python_cookbook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
@@ -11,10 +11,10 @@ chef_version '>= 13.0'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/python_cookbok/issues'
+# issues_url 'https://github.com/<insert_org_here>/python_cookbook/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/python_cookbok'
+# source_url 'https://github.com/<insert_org_here>/python_cookbook'
